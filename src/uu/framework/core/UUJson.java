bc456a98d2d44131b43bb01778d4fcf06901c766
Ryan DeVore <ryan@threejacks.com>
2014-03-28 23:27:25 -0700

    Added overloads that allow a default value. Added calls to JSONObject.has(...) to check for the existence of a key before trying to fetch its value.

--- src/uu/framework/core/UUJson.java
+++ src/uu/framework/core/UUJson.java
@@ -20,54 +20,127 @@ public final class UUJson
 
     public static final String safeGetString(final JSONObject json, final String key)
     {
+        return safeGetString(json, key, "");
+    }
+
+    public static final String safeGetString(final JSONObject json, final String key, final String defaultValue)
+    {
+        String  val = defaultValue;
+
         try
         {
-            return json.getString(key);
+            if (json.has(key))
+            {
+                val = json.getString(key);
+            }
         }
         catch (JSONException ex)
         {
-            Log.e(LOG_TAG, "Error getting JSON String", ex);
-            return null;
+            Log.v(LOG_TAG, "Error getting JSON String", ex);
+            val = defaultValue;
         }
+
+        return val;
     }
 
     public static final long safeGetLong(final JSONObject json, final String key)
     {
+        return safeGetLong(json, key, 0);
+    }
+
+    public static final long safeGetLong(final JSONObject json, final String key, final long defaultValue)
+    {
+        long val = defaultValue;
+
         try
         {
-            return json.getLong(key);
+            if (json.has(key))
+            {
+                val = json.getLong(key);
+            }
         }
         catch (JSONException ex)
         {
-            Log.e(LOG_TAG, "Error getting JSON Long", ex);
-            return 0;
+            Log.v(LOG_TAG, "Error getting JSON Long", ex);
+            val = defaultValue;
         }
+
+        return val;
     }
 
     public static final int safeGetInt(final JSONObject json, final String key)
     {
+        return safeGetInt(json, key, 0);
+    }
+
+    public static final int safeGetInt(final JSONObject json, final String key, final int defaultValue)
+    {
+        int val = defaultValue;
+
         try
         {
-            return json.getInt(key);
+            if (json.has(key))
+            {
+                val = json.getInt(key);
+            }
         }
         catch (JSONException ex)
         {
-            Log.e(LOG_TAG, "Error getting JSON Int", ex);
-            return 0;
+            Log.v(LOG_TAG, "Error getting JSON Int", ex);
+            val = defaultValue;
         }
+
+        return val;
     }
 
     public static final boolean safeGetBool(final JSONObject json, final String key)
     {
+        return safeGetBool(json, key);
+    }
+
+    public static final boolean safeGetBool(final JSONObject json, final String key, final boolean defaultValue)
+    {
+        boolean val = defaultValue;
+
         try
         {
-            return json.getBoolean(key);
+            if (json.has(key))
+            {
+                val = json.getBoolean(key);
+            }
         }
         catch (JSONException ex)
         {
-            Log.e(LOG_TAG, "Error getting JSON Bool", ex);
-            return false;
+            Log.v(LOG_TAG, "Error getting JSON Boolean", ex);
+            val = defaultValue;
         }
+
+        return val;
+    }
+
+    public static Object safeGet(final JSONObject json, final String key)
+    {
+        return safeGet(json, key, null);
+    }
+
+    public static Object safeGet(final JSONObject json, final String key, final Object defaultValue)
+    {
+        Object val = defaultValue;
+
+        try
+        {
+            if (json.has(key))
+            {
+                val = json.get(key);
+            }
+        }
+        catch (JSONException ex)
+        {
+            Log.v(LOG_TAG, "Error getting JSON Field", ex);
+            val = defaultValue;
+        }
+
+        return val;
     }
 
 
