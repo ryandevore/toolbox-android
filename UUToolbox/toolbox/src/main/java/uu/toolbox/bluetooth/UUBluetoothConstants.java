4732f8b9085b026154533b723aa9823a04b1cc9b
Ryan DeVore <ryan@silverpinesoftware.com>
2016-12-29 23:17:43 -0800

    Add client configuration UUID

--- UUToolbox/toolbox/src/main/java/uu/toolbox/bluetooth/UUBluetoothConstants.java
+++ UUToolbox/toolbox/src/main/java/uu/toolbox/bluetooth/UUBluetoothConstants.java
@@ -1,5 +1,7 @@
 package uu.toolbox.bluetooth;
 
+import java.util.UUID;
+
 /**
  * Common bluetooth related constants
  */
@@ -10,4 +12,9 @@ public class UUBluetoothConstants
      * String formatter for building full 128 UUID's from a 16 bit BTLE shortcode
      */
     public static final String BLUETOOTH_UUID_SHORTCODE_FORMAT = "0000%s-0000-1000-8000-00805F9B34FB";
+
+    /**
+     * Client configuration UUID
+     */
+    public static final UUID CLIENT_CHARACTERISTIC_CONFIGURATION_UUID = UUBluetooth.shortCodeToUuid("2902");
 }
