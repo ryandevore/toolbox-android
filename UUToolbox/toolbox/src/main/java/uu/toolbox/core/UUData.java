a58ad5d439574c7262d42fb4bd5d92984f1a436a
Ryan DeVore <ryan@silverpinesoftware.com>
2017-02-10 16:26:50 -0800

    Add Code comments

--- UUToolbox/toolbox/src/main/java/uu/toolbox/core/UUData.java
+++ UUToolbox/toolbox/src/main/java/uu/toolbox/core/UUData.java
@@ -9,6 +9,14 @@ import android.support.annotation.Nullable;
 @SuppressWarnings("unused")
 public class UUData
 {
+    /**
+     * Extracts a set of bytes from a byte array
+     *
+     * @param source the source byte array
+     * @param offset starting index to copy
+     * @param count number of bytes to copy
+     * @return a byte array, or null if index and count are not in bounds
+     */
     public static @Nullable byte[] subData(final @Nullable byte[] source, final int offset, final int count)
     {
         if (source != null && offset >= 0 && count >= 0 && (offset + count) <= source.length)
@@ -16,6 +24,24 @@ public class UUData
             byte[] dest = new byte[count];
             System.arraycopy(source, offset, dest, 0, count);
             return dest;
+        }
+        else
+        {
+            return null;
+        }
+    }
+
+    /**
+     * Returns a full copy of the byte array
+     *
+     * @param source the source byte array
+     * @return a byte array, or null if source is null
+     */
+    public static @Nullable byte[] copy(final @Nullable byte[] source)
+    {
+        if (source != null)
+        {
+            return subData(source, 0, source.length);
         }
         else
         {
