1c3e87319227f005c063698f0604130f67f84bf2
Ryan DeVore <ryan@silverpinesoftware.com>
2017-02-16 14:21:49 -0800

    Code cleanup

--- UUToolbox/toolbox/src/main/java/uu/toolbox/bluetooth/UUBluetoothConstants.java
+++ UUToolbox/toolbox/src/main/java/uu/toolbox/bluetooth/UUBluetoothConstants.java
@@ -9,7 +9,7 @@ import java.util.UUID;
 @SuppressWarnings("unused")
 public class UUBluetoothConstants
 {
-        /**
+    /**
      * String formatter for building full 128 UUID's from a 16 bit BTLE shortcode
      */
     public static final String BLUETOOTH_UUID_SHORTCODE_FORMAT = "0000%s-0000-1000-8000-00805F9B34FB";
