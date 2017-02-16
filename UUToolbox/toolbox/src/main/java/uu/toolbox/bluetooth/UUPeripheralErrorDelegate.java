ed4871de406a1af85e0e02775ba84727370d5824
Ryan DeVore <ryan@silverpinesoftware.com>
2017-02-16 14:21:58 -0800

    New delegate

--- UUToolbox/toolbox/src/main/java/uu/toolbox/bluetooth/UUPeripheralErrorDelegate.java
+++ UUToolbox/toolbox/src/main/java/uu/toolbox/bluetooth/UUPeripheralErrorDelegate.java
@@ -1,16 +1,18 @@
 package uu.toolbox.bluetooth;
 
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 
 /**
  * Interface for delivering async results from a UUPeripheral action
  */
-public interface UUPeripheralDelegate
+public interface UUPeripheralErrorDelegate
 {
     /**
      * Callback invoked when a peripheral action is completed.
      *
      * @param peripheral the peripheral being interacted with
+     * @param error an error if one occurs
      */
-    void onComplete(final @NonNull UUPeripheral peripheral);
+    void onComplete(final @NonNull UUPeripheral peripheral, final @Nullable UUBluetoothError error);
 }
