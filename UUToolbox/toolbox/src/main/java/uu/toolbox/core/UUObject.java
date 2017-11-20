7d20e633fcdd257d3a8ce144bff5dc28dd097d3b
Ryan DeVore <ryan@silverpinesoftware.com>
2017-11-20 05:59:11 -0800

    Handy helper to call a void method from reflection

---
+++ UUToolbox/toolbox/src/main/java/uu/toolbox/core/UUObject.java
@@ -0,0 +1,32 @@
+package uu.toolbox.core;
+
+import android.support.annotation.NonNull;
+
+import java.lang.reflect.Method;
+
+import uu.toolbox.logging.UULog;
+
+/**
+ * A helpful set of methods for use on the base Object class
+ */
+public class UUObject
+{
+    /**
+     * Safely invokes a void/void method on an object.
+     *
+     * @param target object to invoke the method on
+     * @param method the method name
+     */
+    public static void invokeMethod(@NonNull final Object target, @NonNull final String method)
+    {
+        try
+        {
+            Method m = target.getClass().getMethod(method);
+            m.invoke(target);
+        }
+        catch (Exception ex)
+        {
+            UULog.error(UUObject.class, "invokeMethod", ex);
+        }
+    }
+}
