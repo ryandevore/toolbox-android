a1ea19bb64413be1b01275c283a1e1b5ac14c288
Ryan DeVore <ryan@silverpinesoftware.com>
2018-02-08 11:34:10 -0800

    Delegate for classic bluetooth UUBluetoothSession

---
+++ UUToolbox/toolbox/src/main/java/uu/toolbox/bluetooth/UUBluetoothSessionErrorDelegate.java
@@ -0,0 +1,20 @@
+package uu.toolbox.bluetooth;
+
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+
+import uu.toolbox.bluetooth.classic.UUBluetoothSession;
+
+/**
+ * Interface for delivering async results from a UUBluetoothSession action
+ */
+public interface UUBluetoothSessionErrorDelegate
+{
+    /**
+     * Callback invoked when a session action is completed.
+     *
+     * @param session the session being interacted with
+     * @param error an error if one occurs
+     */
+    void onComplete(final @NonNull UUBluetoothSession session, final @Nullable UUBluetoothSession.UUBluetoothSessionError error);
+}
