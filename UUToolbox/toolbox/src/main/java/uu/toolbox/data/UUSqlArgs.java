27bc7ac8b3d59643b345e1cbfc84d2ee194631fb
Ryan DeVore <ryan@silverpinesoftware.com>
2018-02-22 20:38:00 -0800

    New helper class UUSqlArgs

---
+++ UUToolbox/toolbox/src/main/java/uu/toolbox/data/UUSqlArgs.java
@@ -0,0 +1,12 @@
+package uu.toolbox.data;
+
+//
+// Simple struct to hold arguments that will be passed to SQLite methods
+//
+public class UUSqlArgs
+{
+    public String where;
+    public String[] whereArgs;
+    public String orderBy;
+    public String limit;
+}
