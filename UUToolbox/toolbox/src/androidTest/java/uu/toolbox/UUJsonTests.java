7d2bf91e4f15a84710b8b138fbdfccbd0c7a3e0d
Ryan DeVore <ryan@silverpinesoftware.com>
2018-02-28 20:41:17 -0800

    Stubbed out for experimental code

---
+++ UUToolbox/toolbox/src/androidTest/java/uu/toolbox/UUJsonTests.java
@@ -0,0 +1,44 @@
+package uu.toolbox;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * Created by ryandevore on 2/24/18.
+ */
+
+public class UUJsonTests extends ApplicationTestCase<Application>
+{
+    public UUJsonTests()
+    {
+        super(Application.class);
+    }
+
+
+
+    /*public void test_0000_objectToJson() throws Exception
+    {
+        TestObject o = new TestObject();
+        o.stringField = "UnitTest";
+        o.rawIntField = 57;
+        o.objectIntField = 22;
+        o.anotherString = null;
+        o.stringArray = new String[] { "fee", "fy" ,"fo", "fum" };
+        o.intArray = null;
+
+        JSONObject json = UUJson.objectToJson(o);
+        Assert.assertNotNull(json);
+        String jsonString = json.toString();
+        UULog.debug(getClass(), "objectToJson", "JSON: " + jsonString);
+    }*/
+}
+
+class TestObject
+{
+    String stringField;
+    int rawIntField;
+    Integer objectIntField;
+    String anotherString;
+    String[] stringArray;
+    int[] intArray;
+}
