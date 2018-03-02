c87930965b082629dba92d81f0af5b2ca440e3d9
Ryan DeVore <ryan@silverpinesoftware.com>
2018-03-01 23:46:59 -0800

    Add some base64 helpers

--- UUToolbox/toolbox/src/main/java/uu/toolbox/core/UUString.java
+++ UUToolbox/toolbox/src/main/java/uu/toolbox/core/UUString.java
@@ -2,6 +2,7 @@ package uu.toolbox.core;
 
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.util.Base64;
 
 import java.util.ArrayList;
 import java.util.Collections;
@@ -186,6 +187,17 @@ public class UUString
     public static byte[] stringToAsciiBytes(final String input)
     {
         return stringToByte(input, CHARSET_ISO_8859_1);
+    }
+
+    @NonNull
+    public static String byteToBase64String(@Nullable final byte[] data)
+    {
+        if (data == null)
+        {
+            return "";
+        }
+
+        return Base64.encodeToString(data, Base64.NO_WRAP);
     }
 
     /**
