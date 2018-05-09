0e3b18cf4ff159048b57080b090f19ef16a2a63e
Ryan DeVore <ryan@silverpinesoftware.com>
2018-05-09 16:36:35 -0700

    Added a couple helpers to implement Parcelable

--- UUToolbox/toolbox/src/main/java/uu/toolbox/core/UUJsonConvertible.java
+++ UUToolbox/toolbox/src/main/java/uu/toolbox/core/UUJsonConvertible.java
@@ -1,8 +1,11 @@
 package uu.toolbox.core;
 
+import android.os.Parcel;
 import android.support.annotation.NonNull;
 
 import org.json.JSONObject;
+
+import uu.toolbox.logging.UULog;
 
 public interface UUJsonConvertible
 {
@@ -10,4 +13,24 @@ public interface UUJsonConvertible
 
     @NonNull
     JSONObject toJsonObject();
+
+
+    default void writeJsonToParcel(Parcel dest, int flags)
+    {
+        dest.writeString(toJsonObject().toString());
+    }
+
+    default void fillJsonFromParcel(final Parcel in)
+    {
+        try
+        {
+            String jsonString = in.readString();
+            JSONObject json = new JSONObject(jsonString);
+            fillFromJson(json);
+        }
+        catch (Exception ex)
+        {
+            UULog.debug(getClass(), "fillFromParcel", ex);
+        }
+    }
 }
