82c74341a62814bc4e5a070ae71420a13014ca6c
Ryan DeVore <ryan@silverpinesoftware.com>
2018-06-21 07:58:34 -0700

    Make UUError parcelable and Json convertible

--- UUToolbox/toolbox/src/androidTest/java/uu/toolbox/core/UUJsonTests.java
+++ UUToolbox/toolbox/src/androidTest/java/uu/toolbox/core/UUJsonTests.java
@@ -2,6 +2,7 @@ package uu.toolbox.core;
 
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.test.espresso.core.internal.deps.guava.base.Objects;
 import android.support.test.runner.AndroidJUnit4;
 
 import junit.framework.Assert;
@@ -11,6 +12,8 @@ import org.junit.FixMethodOrder;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.MethodSorters;
+
+import java.util.Iterator;
 
 @RunWith(AndroidJUnit4.class)
 @FixMethodOrder(MethodSorters.NAME_ASCENDING)
@@ -26,6 +29,90 @@ public class UUJsonTests
         Assert.assertEquals(lhs.birthday, rhs.birthday);
         Assert.assertEquals(lhs.average, rhs.average);
         Assert.assertEquals(lhs.isActive, rhs.isActive);
+    }
+
+    private static void assertEquals(@Nullable final JSONObject lhs, @Nullable final JSONObject rhs)
+    {
+        if (lhs == null && rhs != null)
+        {
+            Assert.fail("LHS UserInfo null and RHS UserInfo non null");
+        }
+
+        if (lhs != null && rhs == null)
+        {
+            Assert.fail("RHS UserInfo null and LHS UserInfo non null");
+        }
+
+        if (lhs != null && rhs != null)
+        {
+            Assert.assertEquals(lhs.length(), rhs.length());
+
+            Iterator<String> lhsKeys = lhs.keys();
+            Iterator<String> rhsKeys = lhs.keys();
+            while (lhsKeys.hasNext())
+            {
+                String lhsKey = lhsKeys.next();
+                Object lhsVal = lhs.opt(lhsKey);
+
+                boolean keyFound = false;
+                boolean valEqual = false;
+
+                while (rhsKeys.hasNext())
+                {
+                    String rhsKey = rhsKeys.next();
+                    Object rhsVal = rhs.opt(rhsKey);
+
+                    if (lhsKey.equals(rhsKey))
+                    {
+                        keyFound = true;
+
+                        if (lhsVal instanceof JSONObject && rhsVal instanceof JSONObject)
+                        {
+                            assertEquals((JSONObject)lhsVal, (JSONObject)rhsVal);
+                            valEqual = true; // If not it will assert recursively
+                        }
+                        else
+                        {
+                            valEqual = Objects.equal(lhsVal, rhsVal);
+                        }
+
+                        break;
+                    }
+                }
+
+                Assert.assertTrue(keyFound);
+                Assert.assertTrue(valEqual);
+            }
+        }
+    }
+
+    private static void assertEquals(@Nullable final UUError lhs, @Nullable final UUError rhs)
+    {
+        Assert.assertNotNull(lhs);
+        Assert.assertNotNull(rhs);
+        Assert.assertEquals(lhs.getCode(), rhs.getCode());
+        Assert.assertEquals(lhs.getDomain(), rhs.getDomain());
+
+        Exception exLhs = lhs.getException();
+        Exception exRhs = lhs.getException();
+        if (exLhs == null && exRhs != null)
+        {
+            Assert.fail("LHS Exception null and RHS Exception non null");
+        }
+
+        if (exLhs != null && exRhs == null)
+        {
+            Assert.fail("RHS Exception null and LHS Exception non null");
+        }
+
+        if (exLhs != null && exRhs != null)
+        {
+            Assert.assertEquals(exLhs.getMessage(), exRhs.getMessage());
+            Assert.assertEquals(exLhs.getLocalizedMessage(), exRhs.getLocalizedMessage());
+            Assert.assertEquals(exLhs.getClass(), exRhs.getClass());
+        }
+
+        assertEquals(lhs.getUserInfo(), rhs.getUserInfo());
     }
 
     private static BaseballPlayer randomPlayer()
@@ -48,6 +135,61 @@ public class UUJsonTests
 
         BaseballPlayer deserialized = UUJson.parseJsonObject(BaseballPlayer.class, obj);
         assertEquals(p, deserialized);
+    }
+
+    @Test
+    public void test_0001_testUUError_CodeDomain() throws Exception
+    {
+        UUError input = new UUError("TEST", 57);
+        JSONObject obj = input.toJsonObject();
+        System.out.println(obj.toString());
+
+        UUError deserialized = UUJson.parseJsonObject(UUError.class, obj);
+        assertEquals(input, deserialized);
+    }
+
+    @Test
+    public void test_0002_testUUError_CodeDomainException() throws Exception
+    {
+        UUError input = new UUError("TEST", 0xDEADBEEF);
+        input.setException(new IllegalArgumentException("foo"));
+        JSONObject obj = input.toJsonObject();
+        System.out.println(obj.toString());
+
+        UUError deserialized = UUJson.parseJsonObject(UUError.class, obj);
+        assertEquals(input, deserialized);
+    }
+
+    @Test
+    public void test_0003_testUUError_CodeDomainUserInfo() throws Exception
+    {
+        UUError input = new UUError("TEST", 0xDEADBEEF);
+        input.addUserInfo(57, 99);
+        input.addUserInfo("Name", "Bob");
+        JSONObject obj = input.toJsonObject();
+        System.out.println(obj.toString());
+
+        UUError deserialized = UUJson.parseJsonObject(UUError.class, obj);
+        assertEquals(input, deserialized);
+    }
+
+    @Test
+    public void test_0004_testUUError_CodeDomainUserInfoInner() throws Exception
+    {
+        UUError input = new UUError("TEST", 0xDEADBEEF);
+        input.addUserInfo(57, 99);
+        input.addUserInfo("Name", "Bob");
+
+        JSONObject inner = new JSONObject();
+        inner.put("Testing", "1234");
+        inner.put("Hello", "World");
+        input.addUserInfo("Extra", inner);
+
+        JSONObject obj = input.toJsonObject();
+        System.out.println(obj.toString());
+
+        UUError deserialized = UUJson.parseJsonObject(UUError.class, obj);
+        assertEquals(input, deserialized);
     }
 }
 
--- UUToolbox/toolbox/src/main/java/uu/toolbox/core/UUError.java
+++ UUToolbox/toolbox/src/main/java/uu/toolbox/core/UUError.java
@@ -1,20 +1,30 @@
 package uu.toolbox.core;
 
+import android.os.Parcel;
+import android.os.Parcelable;
 import android.support.annotation.NonNull;
 
-import java.util.HashMap;
+import org.json.JSONObject;
 
 /*
     Android equivalent of NSError.  Simply a container for an error code, domain and a dictionary
     of user information.
 
  */
-public class UUError
+public class UUError implements UUJsonConvertible, Parcelable
 {
+    private enum JsonKeys
+    {
+        code,
+        domain,
+        exception,
+        userInfo
+    }
+
     private int code;
     private String domain;
     private Exception exception;
-    private HashMap<Object, Object> userInfo;
+    private JSONObject userInfo;
 
     public UUError()
     {
@@ -56,55 +66,93 @@ public class UUError
         this.exception = exception;
     }
 
-    public HashMap<Object, Object> getUserInfo()
+    public JSONObject getUserInfo()
     {
         return userInfo;
     }
 
-    public void setUserInfo(HashMap<Object, Object> userInfo)
+    public void setUserInfo(JSONObject userInfo)
     {
         this.userInfo = userInfo;
-    }
-
-    public Object getUserInfoObject(Object key)
-    {
-        if (userInfo != null && key != null && userInfo.containsKey(key))
-        {
-            return userInfo.get(key);
-        }
-        else
-        {
-            return null;
-        }
-    }
-
-    public <T extends Object> T getUserInfoObject(final Class<T> type, Object key)
-    {
-        T val = null;
-
-        try
-        {
-            Object obj = getUserInfoObject(key);
-            if (obj != null && type != null && obj.getClass().isAssignableFrom(type))
-            {
-                val = (T) obj;
-            }
-        }
-        catch (Exception ex)
-        {
-            val = null;
-        }
-
-        return val;
     }
 
     public void addUserInfo(Object key, Object value)
     {
         if (userInfo == null)
         {
-            userInfo = new HashMap<>();
+            userInfo = new JSONObject();
         }
 
-        userInfo.put(key, value);
+        UUJson.safePut(userInfo, key, value);
+    }
+
+    public Object getUserInfoField(@NonNull final Object key)
+    {
+        return UUJson.safeGet(userInfo, key);
+    }
+
+
+    ////////////////////////////////////////////////////////////////////////////////////////////////
+    // UUJsonConvertible
+    ////////////////////////////////////////////////////////////////////////////////////////////////
+
+    @Override
+    public void fillFromJson(@NonNull final JSONObject json)
+    {
+        code = UUJson.safeGetInt(json, JsonKeys.code);
+        domain = UUJson.safeGetString(json, JsonKeys.domain);
+
+        byte[] exceptionBytes = UUJson.safeGetDataFromBase64String(json, JsonKeys.exception);
+        exception = UUData.deserializeObject(Exception.class, exceptionBytes);
+
+        userInfo = UUJson.safeGetJsonObject(json, JsonKeys.userInfo);
+    }
+
+    @NonNull
+    @Override
+    public JSONObject toJsonObject()
+    {
+        JSONObject json = new JSONObject();
+
+        UUJson.safePut(json, JsonKeys.code, code);
+        UUJson.safePut(json, JsonKeys.domain, domain);
+        UUJson.safePut(json, JsonKeys.exception, UUString.byteToBase64String(UUData.serializeObject(exception)));
+        UUJson.safePut(json, JsonKeys.userInfo, userInfo);
+
+        return json;
+    }
+
+    ////////////////////////////////////////////////////////////////////////////////////////////////
+    // Parcelable Implementation
+    ////////////////////////////////////////////////////////////////////////////////////////////////
+
+    @Override
+    public int describeContents()
+    {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags)
+    {
+        writeJsonToParcel(dest, flags);
+    }
+
+    public static final Parcelable.Creator<UUError> CREATOR = new Parcelable.Creator<UUError>()
+    {
+        public UUError createFromParcel(Parcel in)
+        {
+            return new UUError(in);
+        }
+
+        public UUError[] newArray(int size)
+        {
+            return new UUError[size];
+        }
+    };
+
+    private UUError(final Parcel in)
+    {
+        fillJsonFromParcel(in);
     }
 }
